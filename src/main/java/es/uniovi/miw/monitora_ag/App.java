17dbf980f498e62aa35887ea34b06ffb73d090b9
Pablo Escalada <diesire@gmail.com>
2014-04-26 20:19:21 +0200

    Add logging support

--- src/main/java/es/uniovi/miw/monitora_ag/App.java
+++ src/main/java/es/uniovi/miw/monitora_ag/App.java
@@ -1,11 +1,16 @@
 package es.uniovi.miw.monitora_ag;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  * 
  */
 public class App {
+	static Logger logger = LoggerFactory.getLogger(App.class);
 	public static void main(String[] args) {
+	    logger.debug("Logging enabled");
 		System.out.println("Hello world!");
 	}
 }
