41717c9e7200f57c462fda935fb5176db7e4d651
Pablo Escalada <diesire@gmail.com>
2014-04-29 20:22:28 +0200

    Delete Foo service

--- src/test/java/es/uniovi/miw/monitora_ag/AppTest.java
+++ src/test/java/es/uniovi/miw/monitora_ag/AppTest.java
@@ -1,42 +1,34 @@
 package es.uniovi.miw.monitora_ag;
 
-import static org.junit.Assert.assertEquals;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import es.uniovi.miw.monitora.core.model.Ack;
-import es.uniovi.miw.monitora.core.model.Foo;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-
-	@Test
-	public void testApp() {
-		Client restClient = ClientBuilder.newClient();
-		WebTarget target = restClient
-				.target("http://localhost:8080/monitora_sv/rest/");
-		WebTarget resourceTarget = target.path("c2/test");
-
-		Foo foo = (Foo) resourceTarget.request()
-				.header("Content-Type", MediaType.TEXT_XML).get(Foo.class);
-		assertEquals("OK", foo.getBar());
-	}
+	static Logger logger = LoggerFactory.getLogger(App.class);
 	
+	@Test
 	public void testPing() {
 		Client restClient = ClientBuilder.newClient();
+		logger.debug("Ping");
 		WebTarget target = restClient
 				.target("http://localhost:8080/monitora_sv/rest/");
-		WebTarget resourceTarget = target.path("c2/ping");
+		String clientId = "AppTestClient";
+		WebTarget resourceTarget = target.path("c2/ping/" + clientId);
 
-		Ack	ack = (Ack) resourceTarget.request()
-				.header("Content-Type", MediaType.TEXT_XML).get(Ack.class);
-		//assertEquals("OK", ack.getUpdate());
+		Ack ack = (Ack) resourceTarget.request()
+				.header("Content-Type", MediaType.APPLICATION_JSON).get(Ack.class);
+		logger.debug("ack {}", ack.getUpdate().getTime());
+		// assertEquals("OK", ack.getUpdate());
 	}
 }
