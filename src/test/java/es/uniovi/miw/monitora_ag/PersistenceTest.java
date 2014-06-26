881f6c1494efe8edafd855305c7b44a13dbc22a9
Pablo Escalada <diesire@gmail.com>
2014-06-26 14:19:25 +0200

    Fix persistence tests

--- src/test/java/es/uniovi/miw/monitora_ag/PersistenceTest.java
+++ src/test/java/es/uniovi/miw/monitora_ag/PersistenceTest.java
@@ -1,6 +1,7 @@
 package es.uniovi.miw.monitora_ag;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -10,13 +11,18 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-import org.junit.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import es.uniovi.miw.monitora.agent.model.Agente;
 import es.uniovi.miw.monitora.agent.model.Cliente;
 
 public class PersistenceTest {
 
+	static Logger logger = LoggerFactory.getLogger(PersistenceTest.class);
 	private EntityManagerFactory factory;
 	private Agente agente;
 	private Cliente cliente;
@@ -24,6 +30,7 @@ public class PersistenceTest {
 
 	@Before
 	public void setUp() {
+		logger.debug("setUp");
 		factory = Persistence.createEntityManagerFactory("monitora_ag");
 		graph = createGraph();
 		persistGraph(graph);
@@ -31,11 +38,11 @@ public class PersistenceTest {
 
 	@After
 	public void tearDown() {
-		removeGraph(graph);
+		logger.debug("tearDown");
+		removeGraph();
 		factory.close();
 	}
 
-	@Test
 	public void testAgente() {
 		EntityManager mapper = factory.createEntityManager();
 		EntityTransaction trx = mapper.getTransaction();
@@ -43,20 +50,36 @@ public class PersistenceTest {
 
 		Agente ag = (Agente) mapper.merge(agente);
 
-		//asserts
+		// asserts
 		assertNotNull(ag.getDestinos());
 
 		trx.commit();
 		mapper.close();
 	}
 
+	@Test
+	public void testCliente() {
+		EntityManager mapper = factory.createEntityManager();
+		EntityTransaction trx = mapper.getTransaction();
+		trx.begin();
+
+		Cliente cl = (Cliente) mapper.merge(cliente);
+
+		assertEquals("Cliente", cl.getNombre());
+
+		trx.commit();
+		mapper.close();
+	}
+
 	private void persistGraph(List<Object> graph) {
+		logger.debug("persistGraph {}", graph);
 		EntityManager mapper = factory.createEntityManager();
 		EntityTransaction trx = mapper.getTransaction();
 		trx.begin();
 
 		for (Object o : graph) {
 			mapper.persist(o);
+			logger.debug("\t + {}", o);
 		}
 
 		trx.commit();
@@ -64,26 +87,37 @@ public class PersistenceTest {
 	}
 
 	private List<Object> createGraph() {
+		logger.debug("createGraph");
 		List<Object> res = new LinkedList<Object>();
-		agente = new Agente();
 		cliente = new Cliente();
-		
-		agente.setCliente(cliente);
-		cliente.addAgente(agente);		
+		cliente.setNombre("Cliente");
 
-		res.add(agente);
 		res.add(cliente);
 
+		logger.debug("\t -> {}", res);
 		return res;
 	}
 
-	private void removeGraph(List<Object> graph) {
+	private List<Object> mergeGraph(EntityManager mapper) {
+		logger.debug("mergeGraph (from mapper {}), mapper");
+		List<Object> res = new LinkedList<Object>();
+		Cliente cl = mapper.merge(cliente);
+
+		res.add(cl);
+
+		logger.debug("\t -> {}", res);
+		return res;
+	}
+
+	private void removeGraph() {
+		logger.debug("removeGraph");
 		EntityManager mapper = factory.createEntityManager();
 		EntityTransaction trx = mapper.getTransaction();
 		trx.begin();
 
-		for (Object o : graph) {
+		for (Object o : mergeGraph(mapper)) {
 			mapper.remove(o);
+			logger.debug("\t - {}", o);
 		}
 
 		trx.commit();
