1f307de4b69c0e73e27ce0260afc10637ba2d305
Pablo Escalada <diesire@gmail.com>
2014-12-08 10:37:24 +0100

    Add App tests

---
+++ src/test/java/es/uniovi/miw/monitora_ag/AppTest.java
@@ -0,0 +1,43 @@
+package es.uniovi.miw.monitora_ag;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import es.uniovi.miw.monitora.agent.App;
+import es.uniovi.miw.monitora.agent.Status;
+
+public class AppTest {
+
+	App app;
+	String[] args = { "App" };
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test(expected = Exception.class)
+	public void test() throws Exception {
+		app = new App(args);
+		try {
+			app.debug();
+			assertTrue(true);
+
+			app.help();
+			assertTrue(true);
+
+			app.exit();
+			assertTrue(true);
+		} catch (InterruptedException e) {
+			fail();
+		}
+
+		throw new Exception("Exit can´t call System.exit()");
+	}
+}
