b564ff16134e4e80e5c1302e45e188eeb53661ae
Pablo Escalada <diesire@gmail.com>
2015-06-09 11:18:10 +0200

    Add persistence layer

--- src/main/java/es/uniovi/miw/monitora/agent/persistence/DBManager.java
+++ src/main/java/es/uniovi/miw/monitora/agent/persistence/DBManager.java
@@ -1,4 +1,4 @@
-package es.uniovi.miw.monitora.agent;
+package es.uniovi.miw.monitora.agent.persistence;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,6 +7,8 @@ import org.hsqldb.Server;
 import org.hsqldb.persist.HsqlProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import es.uniovi.miw.monitora.agent.core.Status;
 
 public class DBManager {
 
@@ -17,13 +19,14 @@ public class DBManager {
 	private Connection dbConn = null;
 	private Status status = Status.CREATED;
 
-	public void startDBServer() {
+	public void startDBServer() throws Exception {
 
 		logger.debug("startDBServer");
 
 		HsqlProperties props = new HsqlProperties();
 		props.setProperty("server.database.0", "file:" + dbLocation + "mydb;");
 		props.setProperty("server.dbname.0", "monitoraag");
+		props.setProperty("server.port", 9002);
 		server = new org.hsqldb.Server();
 
 		try {
@@ -32,45 +35,41 @@ public class DBManager {
 			setStatus(Status.RUNNING);
 		} catch (Exception e) {
 			logger.error("Error starting DB server", e);
+			throw new Exception(e);
 		}
 	}
 
 	public void stopDBServer() {
-
-		logger.debug("stopDBServer");
-		server.stop();
-		setStatus(Status.STOPPED);
+		if (status == Status.RUNNING) {
+			logger.debug("stopDBServer");
+			server.stop();
+			setStatus(Status.STOPPED);
+		}
 	}
 
-	public Connection getDBConn() {
-
+	public Connection getDBConn() throws Exception {
 		try {
-
 			Class.forName("org.hsqldb.jdbcDriver");
 			dbConn = DriverManager.getConnection(
 					"jdbc:hsqldb:hsql://localhost/monitoraag", "SA", "");
 		} catch (Exception e) {
-
 			logger.error("Error getting DB connection", e);
+			throw new Exception(e);
 		}
 
 		return dbConn;
 	}
 
 	public Status getStatus() {
-
 		return status;
 	}
 
 	protected void setStatus(Status status) {
-
 		this.status = status;
 	}
 
 	public void closeDBServer() {
-
 		if (getStatus() == Status.RUNNING) {
-
 			stopDBServer();
 		}
 
