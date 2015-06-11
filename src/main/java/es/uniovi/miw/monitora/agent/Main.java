967a7991977b10bc769b71554201a4e2764d0cc8
Pablo Escalada <diesire@gmail.com>
2015-06-11 17:03:23 +0200

    Change persistence layer

--- src/main/java/es/uniovi/miw/monitora/agent/Main.java
+++ src/main/java/es/uniovi/miw/monitora/agent/Main.java
@@ -1,9 +1,10 @@
 package es.uniovi.miw.monitora.agent;
 
 import es.uniovi.miw.monitora.agent.core.MonitoraAgent;
+import es.uniovi.miw.monitora.server.model.exceptions.BusinessException;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws BusinessException {
 		MonitoraAgent ag = new MonitoraAgent();
 		try {
 			ag.start();
--- src/main/java/es/uniovi/miw/monitora/agent/core/MonitoraAgent.java
+++ src/main/java/es/uniovi/miw/monitora/agent/core/MonitoraAgent.java
@@ -8,10 +8,12 @@ import org.slf4j.LoggerFactory;
 import es.uniovi.miw.monitora.agent.client.MonitoraClient;
 import es.uniovi.miw.monitora.agent.persistence.DBManager;
 import es.uniovi.miw.monitora.core.api.Ack;
+import es.uniovi.miw.monitora.server.conf.PersistenceFactory;
 import es.uniovi.miw.monitora.server.conf.ServicesFactory;
 import es.uniovi.miw.monitora.server.core.impl.agente.FindAgenteById;
 import es.uniovi.miw.monitora.server.model.Agente;
 import es.uniovi.miw.monitora.server.model.exceptions.BusinessException;
+import es.uniovi.miw.monitora.server.persistence.util.PersistenceService;
 
 public class MonitoraAgent implements IMonitoraAgent {
 	public static final int CLIENT_ID = -1; // TODO: get from config file
@@ -20,12 +22,14 @@ public class MonitoraAgent implements IMonitoraAgent {
 
 	private Status status = Status.CREATED;
 	private MonitoraClient client = new MonitoraClient();
-	private DBManager dbm = new DBManager();
+	// private DBManager dbm = new DBManager();
+	private PersistenceService db = PersistenceFactory.getPersistenceService();
 
 	public void start() throws Exception {
 		logger.debug("start");
 		try {
-			dbm.startDBServer();
+			// dbm.startDBServer();
+			db.start();
 			setStatus(Status.RUNNING);
 
 			List<Agente> agentes;
@@ -59,21 +63,22 @@ public class MonitoraAgent implements IMonitoraAgent {
 		this.status = status;
 	}
 
-	public void stop() {
+	public void stop() throws BusinessException {
 		logger.debug("stop");
 		if (getStatus() == Status.RUNNING) {
-			dbm.stopDBServer();
+			// dbm.stopDBServer();
+			db.stop();
 			setStatus(Status.STOPPED);
 		}
 	}
 
-	public void exit() {
+	public void exit() throws BusinessException {
 		logger.debug("exit");
 		if (getStatus() == Status.RUNNING) {
 			stop();
 		}
 
-		dbm.closeDBServer();
+//		dbm.closeDBServer();
 		setStatus(Status.TERMINATED);
 	}
 
