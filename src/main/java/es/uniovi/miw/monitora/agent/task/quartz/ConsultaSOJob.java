39d6985cf8dfcf046eb6c6f0df062af60d723e8c
Pablo Escalada <diesire@gmail.com>
2015-06-22 06:51:04 +0200

    Create table may fail

--- src/main/java/es/uniovi/miw/monitora/agent/task/quartz/ConsultaSOJob.java
+++ src/main/java/es/uniovi/miw/monitora/agent/task/quartz/ConsultaSOJob.java
@@ -2,6 +2,7 @@ package es.uniovi.miw.monitora.agent.task.quartz;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import es.uniovi.miw.monitora.agent.shell.apache.ApacheCommomsExecManager;
 import es.uniovi.miw.monitora.server.model.exceptions.BusinessException;
@@ -20,13 +21,33 @@ public class ConsultaSOJob extends ConsultaJob {
 	@Override
 	protected Object run() throws BusinessException {
 
-		createTable();
+		try {
+			createTable();
+		} catch (Exception e) {
+			// table created
+		}
+
 		insertData(new ApacheCommomsExecManager().run(comando_so));
 
 		return this;
 	}
 
 	private void insertData(String result) throws BusinessException {
+		// Statement stat = null;
+		// try {
+		// stat = db.getConnection().createStatement();
+		// stat.execute(getSql_create());
+		// } catch (SQLException e) {
+		// throw new BusinessException(e);
+		// } finally {
+		// if (stat != null) {
+		// try {
+		// stat.close();
+		// } catch (SQLException e) {
+		// throw new BusinessException(e);
+		// }
+		// }
+		// }
 		PreparedStatement stat = null;
 		try {
 			stat = db.getConnection().prepareStatement(getSql_insert());
